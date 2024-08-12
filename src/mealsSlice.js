3eaef65cf49b0f1ee47133652972f32a46a695ce
karumaj <karumaj@gmail.com>
2024-08-11 23:56:43 -0400

    meals selection

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,10 +4,15 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+   {name: 'Breakfast', cost: 50 , selected: fallse},
+   {name: 'High Tea', cost: 25 , selected: fallse},
+   {name: 'Lunch', cost: 65 , selected: fallse},
+   {name: 'Dinner', cost: 70 , selected: fallse}
+
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+        state[action.paylod].selected = !state[action.payload].selected;
     },
   },
 });
